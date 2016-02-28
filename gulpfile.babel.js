55148cd75885a70e85f76b312dd8c5dc98f56488
izumin5210 <masayuki@izumin.info>
2016-02-28 11:13:35 +0900

    Add gulp tasks for styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,12 +7,39 @@ import path         from "path";
 import config       from "./config";
 
 const $             = loadPlugins();
-const p             = path.join.bind(__dirname)
 
 
-// ---- tasks ------------------------------------------------
+// ---- configurations ------------------------------------------------
+const dest          = path.join(__dirname, config.dest)
+const stylesPath    = path.join.bind(__dirname, config.src.styles.dir);
+const styleFiles    = config.src.styles.entries.map(file => stylesPath(file));
 
-gulp.task("clean", () => {
-  del([p(config.dest)], { dot: true });
+
+// ---- styles ------------------------------------------------
+gulp.task("lint:scss", () => {
+  return gulp.src(stylesPath("**/*.scss"))
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
+    .pipe($.sassLint.failOnError())
 });
 
+gulp.task("build:scss", () => {
+  return gulp.src(styleFiles)
+    .pipe($.sassGlob())
+    .pipe($.sass())
+    .pipe(gulp.dest(dest));
+});
+
+
+// ---- misc ------------------------------------------------
+gulp.task("clean", () => {
+  del([dest], { dot: true });
+});
+
+
+// ---- build ------------------------------------------------
+gulp.task("watch", () => {
+  gulp.watch([stylesPath("**/*.scss")], ["build:scss"]);
+});
+
+gulp.task("build", ["build:scss"]);
