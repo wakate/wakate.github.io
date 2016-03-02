c0475e3fcdd8c0b0f145c1a13c1996a038fb3f39
izumin5210 <masayuki@izumin.info>
2016-03-03 04:04:00 +0900

    Setup styleguide generator

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import loadPlugins  from "gulp-load-plugins";
 import del          from "del";
 import path         from "path";
 import browserSync  from "browser-sync";
+import kss          from "kss";
 
 import config       from "./config";
 
@@ -15,8 +16,18 @@ const reload        = browserSync.reload;
 const p             = path.join;
 const src           = path.join(__dirname, "source");
 const dest          = path.join(__dirname, config.dest);
+const styleEntries  = config.src.styles.entries;
 const stylesPath    = path.join.bind(__dirname, config.src.styles.dir);
-const styleFiles    = config.src.styles.entries.map(file => stylesPath(file));
+const styleFiles    = styleEntries.map(file => stylesPath(file));
+const styleguideTemplate  = path.join(__dirname, config.src["styleguide-template"].dir)
+
+const kssOpts = {
+  source:       stylesPath(),
+  destination:  `${dest}/styleguide`,
+  mask:         '"*.scss"',
+  template:     styleguideTemplate,
+  css:          styleEntries.map((entry) => `/${entry.replace(/\.scss$/, ".css")}`)
+};
 
 
 // ---- styles ------------------------------------------------
@@ -26,6 +37,14 @@ gulp.task("lint:scss", () => {
     .pipe($.sassLint.format())
     .pipe($.sassLint.failOnError())
 });
+
+gulp.task("build:styleguide", $.shell.task([
+  `\$(npm bin)/kss-node <%= opts %>`
+], {
+  templateData: {
+    opts: Object.keys(kssOpts).map((k) => `--${k} ${kssOpts[k]}`).join(" ")
+  }
+}));
 
 gulp.task("build:scss", () => {
   return gulp.src(styleFiles)
@@ -55,8 +74,8 @@ gulp.task("watch", () => {
     reloadDelay: 2000
   });
 
-  gulp.watch([stylesPath("**/*.scss")], ["build:scss"]);
+  gulp.watch([stylesPath("**/*.scss")], ["build:scss", "build:styleguide"]);
   gulp.watch([path.join(src, "**/*")], reload);
 });
 
-gulp.task("build", ["build:scss"]);
+gulp.task("build", ["build:scss", "build:styleguide"]);
