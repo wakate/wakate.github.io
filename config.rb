53268b4f8e5ac5dee66f6deceb7b66f87d5b56e9
izumin5210 <masayuki@izumin.info>
2016-03-28 17:09:29 +0900

    Ignroe styleguide files

--- config.rb
+++ config.rb
@@ -81,10 +81,13 @@ page "/feed.xml", layout: false
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
-  ignore 'styleguide/**/*'
+  # TODO: styleguilde系のファイルが残っててつらい
+  ignore "/styleguide/**/*"
+  ignore "/assets/styleguide-template/**/*"
+  ignore "/assets/styles/**/*"
 end
