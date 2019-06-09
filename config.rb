b476bd488801995b6db8a81cb89e87fcf39a008b
kurochan <kuro@kurochan.org>
2019-06-09 14:08:23 +0900

    enable taglink

--- config.rb
+++ config.rb
@@ -42,7 +42,7 @@ activate :blog do |blog|
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
+  blog.taglink = "tags/{tag}.html"
   blog.layout = "layouts/posts"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
